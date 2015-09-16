bab96b2edca9e89daa6d6e64436a0a7697b1c9b1
dayanarincon <dayana_diaz@misena.edu.co>
2015-09-16 17:48:21 -0500

    hl

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>Ejercico matriculas</title>
+		<script type="js/jquery-2.1.4.min.js"></script>
+
+	</head>
+
+	<body>
+
+		<h1><strong>Bienvenido!!</strong></h1>
+		<h3>Por favor ingrese los siguientes datos: </h3>
+
+		Nombre <input type="text" name="nombre"><br/><br/>
+		Apellido <input type="text" name ="apellido"><br/><br/>
+		Precio Base <input type="text" name="valorbase"><br/><br/>
+
+		<h3>Seleccione el curso: </h3>
+
+		<form action="#">
+		<select id="seleccioneGrado" name="seleccioneGrado">
+			<option value="Seleccione">Seleccione:</option>
+			<option value="Primero">Primero</option>
+			<option value="Segundo">Segundo</option>
+		</select>	
+		<div id="resultado"></div>
+		</form>
+
+		if (select == "Primero"){
+		<form action="#">
+		<select id="seleccionPrimeroA" name ="seleccioneMateria"></select>
+			<option value="Seleccione">Seleccione:</option>
+			<option value="Danzas">Danzas</option>
+			<option value="Teatro">Teatro</option>
+			<option value="Dibujo">Dibujo</option>
+		</form>
+
+		<form action="#">
+		<select id="seleccionPrimeroB" name ="seleccioneMateria"></select>
+			<option value="Seleccione">Seleccione:</option>
+			<option value="Danzas">Danzas</option>
+			<option value="Musica">Musica</option>
+
+		</form>
+
+		<form action="#">
+		<select id="seleccionPrimeroC" name ="seleccioneMateria"></select>
+			<option value="Seleccione">Seleccione:</option>
+			<option value="Teatro">Teatro</option>
+			<option value="Dibujo">Dibujo</option>
+			<option value="Musica">Musica</option>
+		</form>
+
+		<form action="#">
+		<select id="seleccionSegundoA" name ="seleccioneMateria"></select>
+			<option value="Seleccione">Seleccione:</option>
+			<option value="Teatro">Teatro</option>
+			<option value="Dibujo">Dibujo</option>
+		</form>
+		}
+
+
+
+		<form action="#">
+		<select id="seleccionSegundoC" name ="seleccioneMateria"></select>
+			<option value="seleccione">Seleccione:</option>
+			<option value="Teatro">Teatro</option>
+			<option value="Musica">Musica</option>
+		</form>
+
+
+		<script>
+
+			$("#seleccioneGrado").change(function()){
+				var gradoValor = $("#seleccioneGrado").val();
+				$.ajax({
+					method: "POST",
+					url: "matricula.php",
+					data:{ grado:gradoValor}
+				})
+
+				.done(function(msg){
+					$("#resultado").html(msg);
+				})
+			});
+
+		</script>
+
+		<script>
+
+			$("#seleccioneMateria").change(function()){
+				var primeroValor = $("#seleccioneMateria").val();
+				$.ajax({
+					method: "POST",
+					url: "matricula.php",
+					data:{ primero:primeroValor}
+				})
+
+				.done(function(msg){
+					$("#resultado").html(msg);
+				})
+			});
+
+		</script>
+
+	</body>
+
+</html>
---
+++ matricula.php
@@ -0,0 +1,59 @@
+<?php
+
+$Grado= $_POST['grado'];
+
+$arrayprimero = array('A', 'B', 'C');
+$arraysegunddo = array('A', 'C');
+$arrRecorrer;
+
+$Primero= $_POST['primero']
+$arrayprimA = array('Danzas', 'Teatro', 'Dibujo');
+$arrayprimB = array('Danzas','Musica');
+$arrayprimC = array('Teatro','Dibujo' ,'Musica');
+
+$arraysegA = array('Teatro', 'Dibujo');
+$arraysegC = array('Teatro','Dibujo');
+
+$curso= array('Dibujo','Teatro','Musica','Danzas')
+
+$Selccion= "<strong>Seleccione la clase: </strong>";
+
+	if($Grado== 'Primero'){
+		$arrRecorrer=$arrayprimero;
+
+		$valorbase='valorbase';
+		$total= $valorbase + 35000;
+
+
+		if ($arrayprimero == 'A'){
+			$arrRecorrer=$arrayprimA;
+
+			if ($curso== 'Danzas') {
+				$total= $total+($valorbase+)
+
+			}
+
+
+		}else if($arrayprimero == 'B'){
+			$arrRecorrer=$arrayprimB;
+
+		}else if ($arrayprimero=='C'){
+			$arrRecorrer=$arrayprimC;
+		}
+
+
+
+	}else if ($Grado== 'Segundo'){
+		$arrRecorrer=$arraysegunddo;
+
+		$valorbase='valorbase';
+		$total= $valorbase + 70000;
+		echo $total;
+
+		if($arraysegunddo== 'A'){
+
+		}
+
+	}
+
+?>
